c30eb6c1f6490c7410376dc10506e0b553af0a96
RaquelPinho <rdpinho@ucdavis.edu>
2022-06-09 11:00:51 -0700

    Creating branch to develop the bowtie funciton

---
+++ R/bowtie_group_report.R
@@ -0,0 +1,18 @@
+#' Title
+#'
+#' @param path
+#' @param suffix
+#' @param samples
+#'
+#' @return
+#' @export
+#'
+#' @examples
+bowtie_group_report <- function(path = NULL, suffix = "log", samples = NULL) {
+
+}
+
+
+.import_text_from_log <- function(file) {
+
+}

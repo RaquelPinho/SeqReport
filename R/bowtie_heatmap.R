5143550aaadc97585cc3862583807757feb3a41d
RaquelPinho <rdpinho@ucdavis.edu>
2022-06-16 12:26:02 -0700

    finished the heatmap for bowtie

--- R/bowtie_heatmap.R
+++ R/bowtie_heatmap.R
@@ -21,7 +21,8 @@ bowtie_heatmap <- function(dt_bowtie = NULL,
                                  headers = "all",
                                  samples = NULL,
                                  value = "raw",
-                                 filter_samples = NULL
+                                 filter_samples = NULL,
+                                 bar_plot = FALSE
                                  ) {
   # Checking if table exists
   if (is.null(dt_bowtie)) {
@@ -75,50 +76,58 @@ bowtie_heatmap <- function(dt_bowtie = NULL,
   colors = viridis::viridis(256, option = "D", direction = -1)
 
   # Heatmap
+  # Annotations
+  # Deciding colors
+  col_fun2 <- circlize::colorRamp2(c(0,100, 2000, 100000, 1000000),
+                                   c("#FDE725FF", "#5DC863FF", "#21908CFF", "#2C728EFF", "#440154FF"))
+  ## total reads barplot
   if (total_reads == TRUE) {
     if ("total_reads" %in% colnames(dt_int)) {
-      bar_ha <- ComplexHeatmap::HeatmapAnnotation(
-        T_reads = anno_barplot(as.numeric(dt_int$total_reads),
+      bar_ha <- ComplexHeatmap::rowAnnotation(
+        T_reads = ComplexHeatmap::anno_barplot(as.numeric(dt_int$total_reads),
                                gp = grid::gpar(fill = "red")
         )
+      )
+      t_reads <- as.numeric(format(dt_int$total_reads, scientific = TRUE,
+                        digits = 1))
+      text_reads <- as.character(t_reads)
+
+      Tr_ha <- ComplexHeatmap::rowAnnotation(
+        T_reads = ComplexHeatmap::anno_text(paste0("  ", text_reads),
+                                           just = "left",
+                                           gp = grid::gpar(fontsize = 9,
+                                                          fill = col_fun2(t_reads),
+                                                          col = "white",
+                                                          border = "white"),
+                                           show_name = TRUE),
+                                           annotation_name_rot = 0,
+                                           width = (ComplexHeatmap::max_text_width(text_reads)*1.2)
+
       )
 
     } else {
       stop("total_sequences = TRUE, but there is no column 'total_reads'.")
     }
-  } else {
-   bar_ha <- NULL
   }
-
   if (alignment_rate == TRUE) {
-      if ("alignment_rate" %in% colnames(fastqc_table)) {
-        text_ha <- ComplexHeatmap::HeatmapAnnotation(
-          Alignment_rate = anno_text(paste0(dt_int$alignment_rate, "%"))
-          )
+      if ("alignment_rate" %in% colnames(dt_int)) {
+        text_ha <- ComplexHeatmap::rowAnnotation(
+          Al_rate = ComplexHeatmap::anno_text(paste0(dt_int$alignment_rate, "%"),
+                                                     gp = grid::gpar(fontsize = 9),
+                                                     show_name = TRUE,
+                                                     just = "left"),
+          annotation_name_rot = 0
 
+          )
 
       } else {
         stop("alignment_rate = TRUE, but there is no column 'alignment_rate'.")
       }
-  } else {
-    text_ha <- NULL
   }
-
   if (headers == "main") {
     factor <- "Main"
   } else {
     headers_i_m <- grep("_H", colnames(mat))
-    mat_list <- list()
-    for (i in 1:length(headers_i_m)) {
-      if (i == 1) {
-        mat_list[[i]] <- mat[,seq(1,(headers_i_m[i+1]-1))]
-      } else if (1 < i & headers_i_m[i] < max(headers_i_m)) {
-        mat_list[[i]] <- mat[,seq(headers_i_m[i],(headers_i_m[i+1]-1))]
-      } else if (headers_i_m[i] >= max(headers_i_m)) {
-        mat_list[[i]] <- mat[,seq(headers_i_m[i], ncol(mat))]
-      }
-    }
-
     factor <- c()
     for (i in 1:length(headers_i_m)) {
       if (i == 1) {
@@ -130,70 +139,66 @@ bowtie_heatmap <- function(dt_bowtie = NULL,
       }
     }
   }
+  # Plotting
+  # Increasing legend size
+  lgd = list(col_fun = col_fun2, title = "N_reads",
+                               at = c(0,100, 1500, 3e+05, 5e+05, 1e+06),
+                               break_dist = c(1, 2, 3, 2, 3),
+                               legend_height = grid::unit(6, "cm"))
+  # Plot without the annotations
+  htmp <- ComplexHeatmap::Heatmap(mat,
+                                  name = "N reads",
+                                  col = col_fun2,
+                                  rect_gp = grid::gpar(col = "white", lwd = 1),
+                                  cluster_columns = FALSE,
+                                  cluster_rows = FALSE,
+                                  row_names_side = "left",
+                                  column_names_side = "top",
+                                  column_split = factor,
+                                  row_names_gp = grid::gpar(fontsize = 10),
+                                  column_names_max_height = grid::unit(11, "cm"),
+                                  column_names_gp = grid::gpar(fontsize = 10),
+                                  column_gap = grid::unit(3, "mm"),
+                                  heatmap_legend_param = lgd
+  )
   if(alignment_rate == FALSE & total_reads == FALSE) {
-    htmp <- ComplexHeatmap::Heatmap(mat,
-                                    name = "N reads",
-                                    col = col_fun,
-                                    rect_gp = grid::gpar(col = "white", lwd = 1),
-                                    cluster_columns = FALSE,
-                                    cluster_rows = FALSE,
-                                    row_names_side = "left",
-                                    column_names_side = "top",
-                                    column_split = factor,
-                                    row_names_gp = grid::gpar(fontsize = 10),
-                                    column_names_max_height = grid::unit(11, "cm"),
-                                    column_names_gp = grid::gpar(fontsize = 10),
-                                    column_gap = grid::unit(3, "mm")
-    )
+    htmp <- htmp
+  } else if (alignment_rate == TRUE & total_reads == FALSE) {
+    htmp <- htmp + text_ha
+  } else if (alignment_rate == FALSE & total_reads == TRUE) {
+    if (bar_plot = FALSE) {
+      htmp <- htmp + Tr_ha
+    } else {
+      htmp <- htmp + bar_ha
+    }
+  } else if (alignment_rate == TRUE & total_reads == TRUE) {
+    if (bar_plot = FALSE) {
+      htmp <- htmp + Tr_ha + text_ha
+    } else {
+      htmp <- htmp + bar_ha + text_ha
+    }
   }
+  return(htmp)
 }
-#col_fun <- circlize::colorRamp2(c(0, 30, 50,100, 180, 1500, 2000, 220000, 250000, 270000, 300000,  600000,650000, 1500000), hcl_palette = "viridis", reverse = TRUE )
-#col_fun <- circlize::colorRamp2(c(0,50, 180, 1500, 2000, 25000,30000, 60000, 120000), hcl_palette = "viridis", reverse = TRUE )
-#scales::show_col( col_fun(c(0, 10, 20, 30, 50,100, 180, 1500, 2000, 220000, 250000, 270000, 300000, 550000, 600000,650000, 1000000,1500000)))
-#viridis::viridis(9)
-#[1] "#440154FF" "#472D7BFF" "#3B528BFF" "#2C728EFF" "#21908CFF"
-#[6] "#27AD81FF" "#5DC863FF" "#AADC32FF" "#FDE725FF"
-#col_fun2 <- circlize::colorRamp2(c(0,100, 2000, 100000, 1000000), c("yellow", "green", "darkgreen", "lightblue", "darkblue"))
-#htmp <- ComplexHeatmap::Heatmap(mat,
-#                                name = "N reads",
-#                                col = colors,
-#                                rect_gp = grid::gpar(col = "white", lwd = 1),
-#                                cluster_columns = FALSE,
-#                                cluster_rows = FALSE,
-#                                row_names_side = "left",
-#                                column_names_side = "top"
-#
-#
-#)
-#
-#                                ,
-#                                row_names_side = "left",
-#                                column_names_max_height = grid::unit(11, "cm"),
-#                                column_names_gp = grid::gpar(fontsize = 10),
-#                                row_names_gp = grid::gpar(fontsize = 10),
-#
-#)
-#
-#
-#
-#.perc_fun <- function(x) {
-#  str_col <-  sapply(x, function(ele) {
-#    spl_ele <- unlist(strsplit(ele, " "))
-#    ele <- ifelse(length(spl_ele) == 2,
-#                        spl_ele[2],
-#                        spl_ele[1]
-#    )
-#    return(ele)
-#
-#  })
-#}
-#
-#.value_fun <- function(x) {
-#  str_col <-  sapply(x, function(ele) {
-#    spl_ele <- unlist(strsplit(ele, " "))
-#    ele <- spl_ele[1]
-#    return(ele)
-#
-#  })
-#}
-#
+
+.perc_fun <- function(x) {
+  str_col <-  sapply(x, function(ele) {
+    spl_ele <- unlist(strsplit(ele, " "))
+    ele <- ifelse(length(spl_ele) == 2,
+                        spl_ele[2],
+                        spl_ele[1]
+    )
+    return(ele)
+
+  })
+}
+
+.value_fun <- function(x) {
+  str_col <-  sapply(x, function(ele) {
+    spl_ele <- unlist(strsplit(ele, " "))
+    ele <- spl_ele[1]
+    return(ele)
+
+  })
+}
+
